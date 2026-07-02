078137c29be82c3bf6d9718f75ae2dd4cc9a7d58
Joe <youyaya567023@gmail.com>
2026-07-02 13:38:41 +0800

    Initial Commit

---
+++ Copy_Paste/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Copy_Paste
+//
+//  Created by Joe on 7/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Copy_Paste/Copy_PasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Copy_PasteApp.swift
+//  Copy_Paste
+//
+//  Created by Joe on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Copy_PasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
